ea2ba1720b44abfceef2022f8c78cfb7ced405df
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-27 23:03:22 +0600

    Create Find the Positive Numbers in the Array.c

---
+++ Find the Positive Numbers in the Array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>  
+int main(){  
+int i, size;  
+printf("Enter any array size: ");  
+scanf(“%d”,&size); 
+int arr[size];  
+printf("Enter %d numbers: ", size);  
+for(i=0; i<size; i++){  
+scanf("%d", &arr[i]);
+}
+printf("The array elements are: \n");
+    for(i=0;i<size;i++){
+        printf("%d ", arr[i]); 
+    }
+printf("\nThe odd mumbers are: ");  
+for(i=0; i<size; i++){  
+if(arr[i]>=0){
+     printf("%d ", arr[i]);
+    }
+}  
+return 0;
+}
