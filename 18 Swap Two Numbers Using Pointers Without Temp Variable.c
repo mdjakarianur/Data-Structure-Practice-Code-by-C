36a3034e414f734a13b8ccd2abc32c962620d763
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-28 23:14:37 +0600

    Update 18 Swap Two Numbers Using Pointers Without Temp Variable.c

--- 18 Swap Two Numbers Using Pointers Without Temp Variable.c
+++ 18 Swap Two Numbers Using Pointers Without Temp Variable.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main(){
+int A=20;
+int B=10;
+printf("\nBefore Swaping A = %d, B = %d.\n",A,B);
+
+int *x = &A;
+int *y = &B;
+
+*x = *x+*y;
+*y = *x-*y;
+*x = *x-*y;
+
+printf("\nAfter Swaping  A = %d, B = %d.\n",A,B);
+
+return 0;
+}
 
